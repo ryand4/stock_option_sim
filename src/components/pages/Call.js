7f88137609fb1066117bad4cef70342aa9f7f2c3
ryand4 <97061451+ryand4@users.noreply.github.com>
2024-07-12 20:38:10 -0700

    updates

--- src/components/pages/Call.js
+++ src/components/pages/Call.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import './Call.css'; // Make sure to import the CSS file
+import React, { useState } from 'react';
+import './Call.css';
 
 const Call = () => {
+  const [formData, setFormData] = useState({
+    numContracts: '',
+    contractCost: '',
+    expirationDate: ''
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);  // Set the form as submitted
+  };
+
   return (
     <div className="container">
       <video autoPlay loop muted className="background-video">
@@ -9,12 +29,53 @@ const Call = () => {
         Your browser does not support the video tag.
       </video>
       <div className="content">
-        <h1 className="title">Call Option</h1>
-        <p>Welcome to our call options simulation page. Here, you can explore various strategies and their potential returns.</p>
-        {/* Insert more content as needed */}
+        {!isSubmitted ? (
+          <div className="form-container">
+            <h1 className="title">Call Option</h1>
+            <form onSubmit={handleSubmit}>
+              <label>
+                Number of Contracts:
+                <input
+                  type="number"
+                  name="numContracts"
+                  value={formData.numContracts}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <label>
+                Average Contract Cost/Premium:
+                <input
+                  type="number"
+                  name="contractCost"
+                  step="0.01"
+                  value={formData.contractCost}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <label>
+                Expiration Date:
+                <input
+                  type="date"
+                  name="expirationDate"
+                  value={formData.expirationDate}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        ) : (
+          <div className="result-container">
+            <h1>Contracts: {formData.numContracts}</h1>
+            <h1>Premium: {formData.contractCost}</h1>
+            <h1>Exp Date: {formData.expirationDate}</h1>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Call;
+
+
